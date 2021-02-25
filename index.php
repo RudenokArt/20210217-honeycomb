00e9a24a4cf64a215fafed3c932024eeb61ad6aa
RudenokArt <rudenokart@yandex.ru>
2021-02-25 23:19:39 +0300

    start

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css?<?php echo time() ?>">
+  <title>Document</title>
+</head>
+<body>
+  <div class="comb-container">
+      <?php for ($i=0; $i < 6; $i++) { ?>
+        <div class="comb-double">
+          <div class="comb-block">
+            <div class="comb-tab_left">
+              <div class="comb-tab_border-1"></div>
+              <div class="comb-tab_border-2"></div>
+              <div class="comb-tab_content">
+                <div class="comb-tab_content-top"></div>
+                <div class="comb-tab_content-bottom"></div>
+              </div>
+            </div>
+            <div class="comb-tab_right">
+              <div class="comb-tab_border-1"></div>
+              <div class="comb-tab_border-2"></div>
+              <div class="comb-tab_content">
+                <div class="comb-tab_content-top"></div>
+                <div class="comb-tab_content-bottom"></div>
+              </div>
+            </div>
+          </div>
+          <div class="comb-block">
+            <div class="comb-tab_left">
+              <div class="comb-tab_border-1"></div>
+              <div class="comb-tab_border-2"></div>
+              <div class="comb-tab_content">
+                <div class="comb-tab_content-top"></div>
+                <div class="comb-tab_content-bottom"></div>
+              </div>
+            </div>
+            <div class="comb-tab_right">
+              <div class="comb-tab_border-1"></div>
+              <div class="comb-tab_border-2"></div>
+              <div class="comb-tab_content">
+                <div class="comb-tab_content-top"></div>
+                <div class="comb-tab_content-bottom"></div>
+              </div>
+            </div>
+          </div>
+          </div><?}?>
+        </div>
+
+    </body>
+    </html>
